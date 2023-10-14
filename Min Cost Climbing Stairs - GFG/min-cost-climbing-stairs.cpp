4bdc43b04c739ff87ba90f5d52c0d01a9663f59f
Anupam Anand <74010317+anupam2215@users.noreply.github.com>
2023-10-14 16:52:35 +0530

    Added solution - LeetHub

--- Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.cpp
+++ Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.cpp
@@ -9,24 +9,24 @@ using namespace std;
 //Back-end complete function Template for C++
 
 class Solution {
-    public:
-     int solve(vector<int>&cost , int n , vector<int>&dp ){
-         if(n==0) return cost[0];
-         if(n==1) return cost[1];
-         if(dp[n]!=-1) return dp[n]; //step3
-         
-         //step2
-         
-         dp[n] = cost[n] + min(solve(cost,n-1,dp) , solve(cost , n-2,dp));
-         return dp[n];
-      }
   public:
+  int solve(vector<int>&cost ,int N , vector<int>& dp){
+      if(N==0) return cost[0];
+      if(N==1) return cost[1];
+       
+      //step 3
+      if(dp[N]!= -1) return dp[N];
+      
+      //step 2 storing the value
+      dp[N] = cost[N] + min(solve(cost , N-1 , dp) , solve(cost , N-2 , dp));
+      return dp[N];
+      
+  }
     int minCostClimbingStairs(vector<int>&cost ,int N) {
         //Write your code here
-        //step1-
+        //step1
         vector<int> dp(N+1,-1);
-        
-        int ans = min(solve(cost,N-1,dp) , solve(cost , N-2,dp));
+        int ans = min(solve(cost , N-1 , dp) , solve(cost , N-2 , dp));
         return ans;
     }
 };
