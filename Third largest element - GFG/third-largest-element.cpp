3d71b20ff351557660c7888d033d8245ba29fdaa
Anupam Anand <74010317+anupam2215@users.noreply.github.com>
2023-10-30 14:49:17 +0530

    Added solution - LeetHub

--- Third largest element - GFG/third-largest-element.cpp
+++ Third largest element - GFG/third-largest-element.cpp
@@ -8,8 +8,28 @@ class Solution{
     int thirdLargest(int a[], int n)
     {
          //Your code here
-         sort(a,a+n);
-         return a[n-3];
+        int first=a[0];
+        int second=INT_MIN;
+        int third=INT_MIN;
+         
+        for(int i=1;i<n;i++){
+            if(a[i]>first){
+            third=second;
+            second=first;
+            first=a[i];
+            }
+            
+            else if(a[i]>second){
+                third=second;
+                second=a[i];
+                
+            }
+            
+            else if(a[i]>third){
+                third=a[i];
+            }
+        }
+        return third;
     }
 
 };
