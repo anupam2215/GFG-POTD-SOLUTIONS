b0f530c5d841c50462f4f10ddb7240fa82202f95
Anupam Anand <74010317+anupam2215@users.noreply.github.com>
2023-11-01 13:04:01 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.cpp
+++ Majority Element - GFG/majority-element.cpp
@@ -8,41 +8,37 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
-class Solution {
-public:
-    int majorityElement(int a[], int size) {
-        int candidate = a[0];
-        int count = 1;
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
         
-        for (int i = 1; i < size; i++) {
-            if (a[i] == candidate) {
+        // your code here
+        int candidate=a[0];
+        int count=1;
+        
+        for(int i=1;i<size;i++){
+            if(a[i]==candidate){
                 count++;
-            } else {
+            }else{
                 count--;
-                if (count == 0) {
-                    candidate = a[i];
-                    count = 1;
+                if(count==0){
+                    candidate=a[i];
+                    count++;
                 }
             }
         }
-        
-        // At this point, the candidate should be the majority element
-        // However, you should validate that it appears more than N/2 times
-        count = 0;
-        for (int i = 0; i < size; i++) {
-            if (a[i] == candidate) {
-                count++;
-            }
+        count=0;
+        for(int i=0;i<size;i++){
+            if(a[i]==candidate) count++;
         }
-        
-        if (count > size / 2) {
-            return candidate;
-        }
-        
-        return -1; // No majority element found
+        if(count>size/2) return candidate;
+        return -1;
     }
 };
-
 
 //{ Driver Code Starts.
 
