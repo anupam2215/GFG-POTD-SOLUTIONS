d34b083c0b05db4e8ff7111534f8fa220587bf22
Anupam Anand <74010317+anupam2215@users.noreply.github.com>
2023-12-14 16:03:15 +0530

    Added solution - LeetHub

--- Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.cpp
@@ -12,19 +12,18 @@ class Solution
     int missingNumber(int arr[], int n) 
     { 
         // Your code here
-        for (int i = 0; i < n; i++) {
-            while (arr[i] > 0 && arr[i] <= n && arr[i] != arr[arr[i] - 1]) {
-                swap(arr[i], arr[arr[i] - 1]);
+        for(int i=0;i<n;i++){
+            while(arr[i]>=1 && arr[i]<=n && arr[i]!=arr[arr[i]-1]){
+                swap(arr[i],arr[arr[i]-1]);
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != i + 1) {
-                return i + 1;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=i+1){
+                return i+1;
             }
         }
-
-        return n + 1;
+        return n+1;
+        
     } 
 };
 
