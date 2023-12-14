d8550829807a7f65d9899929c557ab2e14756938
Anupam Anand <74010317+anupam2215@users.noreply.github.com>
2023-12-14 23:21:06 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -16,18 +16,15 @@ class Solution{
         int i=0;
         long long maxi=arr[i];
         
-        for(int i=0; i<n;i++){
-            sum =sum+arr[i];
-            maxi= max(maxi,sum);
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            maxi=max(maxi,sum);
             
-            if(sum<0){
-                sum=0;
-            }
+            if(sum<0) sum=0;
         }
         return maxi;
     }
 };
-
 
 //{ Driver Code Starts.
 
