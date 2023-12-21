56131003e80077fe25d1fbf3fd2ed7a9e2b70703
Anupam Anand <74010317+anupam2215@users.noreply.github.com>
2023-12-21 21:56:59 +0530

    Added solution - LeetHub

--- Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.cpp
+++ Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.cpp
@@ -12,15 +12,7 @@ class Solution
     bool areRotations(string s1,string s2)
     {
         // Your code here
-        // if(s1.size()!=s2.size()) return 0;
-        
-        // s1=s1+s1;
-        
-        // if(s1.find(s2)!=string::npos) 
-        //     return true;
-        // else 
-        //     return false;
-        
+
         if(s1.size()!=s2.size())return 0;
         
         s1=s1+s1;
